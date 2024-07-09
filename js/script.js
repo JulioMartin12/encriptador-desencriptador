0eab712b1692eb93f0c1d1198887dbc26bc549f4
Julio Martin <faria@DESKTOP-TLUUL68>
2024-07-08 23:19:47 -0300

    funciones de encriptar y desencriptar

---
+++ js/script.js
@@ -0,0 +1,65 @@
+const vocales = {
+  a: "ai",
+  e: "enter",
+  i: "imes",
+  o: "ober",
+  u: "ufat",
+};
+
+let palabrasEncriptadas=[];
+
+function encriptar() {
+  let textArea = document.getElementById("textarea-principal");
+  let contenidoTextArea = textArea.value.toLowerCase();
+  let codigo = "";
+  console.log(contenidoTextArea);
+
+  for (const value of contenidoTextArea) {
+    if (value != " ") {
+      codigo += encontrarCodigo(value);
+    } else {
+      codigo += " ";
+    }
+  }
+  console.log();
+ 
+  palabrasEncriptadas = codigo.split(' ');
+  console.log(palabrasEncriptadas);
+}
+
+function encontrarCodigo(val) {
+  for (const clave in vocales) {
+    if (clave == val) {
+      return vocales[clave];
+    }
+  }
+  return val;
+}
+
+function desencriptar(){
+    let textoDesencriptado= document.getElementById('textarea-principal').value;
+    if(palabrasEncriptadas.length == 0 && textoDesencriptado == " " ){
+        console.log("no hay texto para desencriptar");
+    }else{ 
+        for (const clave in vocales) {
+            let expresion = new RegExp( vocales[clave], 'g');
+             textoDesencriptado = textoDesencriptado.replace(expresion, clave);
+            }
+          }
+          console.log( textoDesencriptado);
+    
+
+    return textoDesencriptado;
+}
+
+function limpiarTextArea(){
+    document.getElementById('textarea-principal').value="";
+}
+
+function restaurarTextAreaPrincipal(){
+    if(document.getElementById('textarea-principal').value != ""){
+
+    }else{
+        document.getElementById('textarea-principal').value="Ingrese el texto aqui";
+    }
+}
