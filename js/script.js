a53e70ff2792436aaa1d3932f9a051ee0e09824e
Julio Martin <faria@DESKTOP-TLUUL68>
2024-07-09 22:12:27 -0300

    Realizando responsive tablet y desktop

--- js/script.js
+++ js/script.js
@@ -13,18 +13,27 @@ function encriptar() {
   let contenidoTextArea = textArea.value.toLowerCase();
   let codigo = "";
   console.log(contenidoTextArea);
-
-  for (const value of contenidoTextArea) {
-    if (value != " ") {
-      codigo += encontrarCodigo(value);
-    } else {
-      codigo += " ";
-    }
-  }
   palabrasEncriptadas = codigo.split(' ');
+  console.log(palabrasEncriptadas.length);
+  if(palabrasEncriptadas.length != 0 && contenidoTextArea!="ingrese el texto aquí"){
+    for (const value of contenidoTextArea) {
+      if (value != " ") {
+        codigo += encontrarCodigo(value);
+      } else {
+        codigo += " ";
+      }
+    }
+ 
   console.log(codigo);
+  desabilitarImagen();
   document.getElementById('main__texto__desencriptar__encriptar__titulo').textContent= "El texto Encriptado";
   document.getElementById('main__texto__desencriptar__encriptar__contenido').value=codigo;
+  habilitarBtnCopiar();
+  }else{
+    document.getElementById('main__texto__desencriptar__encriptar__titulo').textContent= "Ningún mensaje fue encontrado";
+    document.getElementById('main__texto__desencriptar__encriptar__contenido').value="Ingresa el texto que desees encriptar o desencriptar.";
+  }
+    
 }
 
 function encontrarCodigo(val) {
@@ -37,20 +46,23 @@ function encontrarCodigo(val) {
 }
 
 function desencriptar(){
-    let textoDesencriptado= document.getElementById('textarea-principal').value;
-    if(palabrasEncriptadas.length == 0 && textoDesencriptado == " " ){
+    let textoDesencriptado = document.getElementById("textarea-principal");
+  let contenidoTextArea =  textoDesencriptado.value.toLowerCase();
+    console.log(palabrasEncriptadas.length)
+    if( palabrasEncriptadas.length == 0 && (contenidoTextArea=="ingrese el texto aqui" ||contenidoTextArea == " ") ){
         console.log("no hay texto para desencriptar");
     }else{ 
         for (const clave in vocales) {
             let expresion = new RegExp( vocales[clave], 'g');
-             textoDesencriptado = textoDesencriptado.replace(expresion, clave);
+            contenidoTextArea = contenidoTextArea.replace(expresion, clave);
             }
+            document.getElementById('main__texto__desencriptar__encriptar__contenido').value=contenidoTextArea;
+            document.getElementById('main__texto__desencriptar__encriptar__titulo').textContent= "El texto Desencriptado";
           }
-          console.log( textoDesencriptado);
+          console.log( contenidoTextArea);
     
-          document.getElementById('main__texto__desencriptar__encriptar__contenido').value=textoDesencriptado;
-          document.getElementById('main__texto__desencriptar__encriptar__titulo').textContent= "El texto Desencriptado";
-    return textoDesencriptado;
+         
+    return contenidoTextArea;
 }
 
 function limpiarTextArea(){
@@ -61,6 +73,52 @@ function restaurarTextAreaPrincipal(){
     if(document.getElementById('textarea-principal').value != ""){
 
     }else{
-        document.getElementById('textarea-principal').value="Ingrese el texto aqui";
+        document.getElementById('textarea-principal').value="Ingrese el texto aquí";
     }
 }
+
+function habilitarBtnCopiar(){
+  var botones = document.getElementsByClassName('main__contenedor__copiar');
+  for (var i = 0; i < botones.length; i++) {
+    botones[i].style.display = "block";
+  }
+
+}
+
+function desabilitarImagen(){
+  var imagen = document.getElementsByClassName('main__principal__derecha__imagen');
+  for (var i = 0; i < imagen.length; i++) {
+    imagen[i].style.display = "none";
+  }
+
+}
+
+
+
+function copiarTexto(){
+  let textarea = document.getElementById("main__texto__desencriptar__encriptar__contenido");
+  let text = textarea.value;
+  let mensaje = document.getElementById("mensaje");
+
+  navigator.clipboard.writeText(text).then(function() {
+      mensaje.style.display = "block";
+      setTimeout(function() {
+          mensaje.style.display = "none";
+      }, 2000); 
+  }).catch(function(err) {
+      console.error('Error al copiar el texto: ', err);
+      mensaje.textContent = "No se pudo copiar el texto";
+      mensaje.style.backgroundColor = "lightcoral";
+      mensaje.style.borderColor = "red";
+      mensaje.style.color = "red";
+      mensaje.style.display = "block";
+      setTimeout(function() {
+          mensaje.style.display = "none";
+          mensaje.textContent = "Texto copiado al portapapeles";
+          mensaje.style.backgroundColor = "lightgreen";
+          mensaje.style.borderColor = "green";
+          mensaje.style.color = "green";
+      }, 2000); 
+  });
+}
+
